96af6c0dedce5aaf6e6d21510751ab13948ce7bb
Serial <Serial@arbad007791.corp.capgemini.com>
2017-03-21 15:45:55 -0300

    se agrega lector

---
+++ src/AnalizadorEstructural/Lector.java
@@ -0,0 +1,40 @@
+package AnalizadorEstructural;
+
+import analizadorLexico.Linea;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedReader;
+
+public class Lector {
+	 Linea lineaLeida = new Linea();
+	 String linea= new String();
+	 
+	 try(BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {	
+		    List<analizadorLexico.Campo> campos= new ArrayList<analizadorLexico.Campo>();
+		    
+		    linea = br.readLine();
+		    
+		    while (linea != null){
+		    	lineaLeida.setUpLinea();
+		    	lineaLeida.setLineaAnalizar(linea);
+		    	lineaLeida.obtenerTokens();
+				campos.add(lineaLeida.generarCampo());
+		    	linea = br.readLine();
+		    }
+	 }catch  (Exception e)  {	
+	throw new Exception();
+		
+	}
+		    
+		    
+		   
+
+	
+		}
+	 
+	 
+
+}
